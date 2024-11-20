4c96c020183cd49419d52b70090f7c836ac4d7c6
Lucas Souza <lucas.rsouza@yduqs.com.br>
2024-11-20 19:38:20 -0300

    atualizando arquitetura

---
+++ Back-end/SocialHub.Data/PostRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialHub.Domain.DTOs.Request;
+using SocialHub.Domain.Entities;
+using SocialHub.Domain.Interfaces.Dapper;
+using SocialHub.Infrastructure.Data;
+
+namespace SocialHub.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Post>> GetAllAsync()
+        {
+            return await _context.Set<Post>().OrderByDescending(p => p.CreatedAt).ToListAsync();
+        }
+
+        public async Task<Post> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<Post>().FindAsync(id);
+        }
+
+        public async Task AddAsync(PostRequestDTO post)
+        {
+            await _context.Set<PostRequestDTO>().AddAsync(post);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
